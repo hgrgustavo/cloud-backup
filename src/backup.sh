a572a01af5c0498708105489685c7ce94b513fdf
hgrgustavo <gustavohgrdev@gmail.com>
2025-11-05 16:48:42 -0300

    feat: add scripts into a respective cronjob

---
+++ src/backup.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source ../.env
+
+mv ~/Design/videos/*.mp4 ~/Design/videos/*.mov -t "$BACKUP_VIDEO_DESTINY"
---
+++ src/directory.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+source ../.env
+
+mkdir -p "$BACKUP_VIDEO_DESTINY"
