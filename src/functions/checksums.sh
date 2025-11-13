c710b7d683ab27bf12933d58ac74755e959c4d84
hgrgustavo <gustavohgrdev@gmail.com>
2025-11-13 17:20:46 -0300

    fix: checksum functions implementation and more descriptive logs

--- src/functions/checksums.sh
+++ src/functions/checksums.sh
@@ -1,19 +1,27 @@
-#!/bin/zsh
+#!/bin/bash
 
 source /home/hgrgustavo/Projects/cloud-backup/.env
 
 generate_checksums() {
-  mkdir -p "$TRIAGE_VIDEO_CHECKSUMS_PATH"
+  mkdir -p "${TRIAGE_VIDEO_CHECKSUMS_PATH}"
 
-  for file in $(find "$TRIAGE_VIDEO_CHECKSUMS_PATH" -type f); do
-    if [[ ! -f "${file}.sha256" ]]; then
-      sha256sum "$file" >"${TRIAGE_VIDEO_CHECKSUMS_PATH}/${file}.sha256"
+  for file in $(find "$TRIAGE_VIDEO_PATH" -type f); do
+    checksum_file="${file}.sha256"
+
+    if [[ ! -f "$checksum_file" ]]; then
+      sha256sum "$file" >"${TRIAGE_VIDEO_CHECKSUMS_PATH}/$checksum_file"
+      chmod a-w "$checksum_file"
     fi
+
   done
 }
 
 verify_checksums() {
-  for file in $(find "$TRIAGE_VIDEO_CHECKSUMS_PATH" -type f); do
-    sha256sum -c "$file" >>"${TRIAGE_VIDEO_CHECKSUMS_PATH}/checksums_log.log"
+  log_file="${TRIAGE_VIDEO_CHECKSUMS_PATH}/checksums_log.log"
+
+  echo "$(date): " >"$log_file"
+
+  for checksum in $(find "$TRIAGE_VIDEO_CHECKSUMS_PATH" -type f -name "*.sha256"); do
+    sha256sum -c "$checksum_file" >>"$log_file" 2>&1
   done
 }
