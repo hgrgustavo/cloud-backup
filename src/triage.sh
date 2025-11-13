bd9c17357fd3006dc61facfcb8cf8b05fbd65d7e
hgrgustavo <gustavohgrdev@gmail.com>
2025-11-13 17:22:20 -0300

    fix: wrong functions usage

--- src/triage.sh
+++ src/triage.sh
@@ -4,7 +4,7 @@ source /home/hgrgustavo/Projects/cloud-backup/.env
 source /home/hgrgustavo/Projects/cloud-backup/src/functions/checksums.sh
 
 mv /home/hgrgustavo/Downloads/*.mp4 -t "$TRIAGE_VIDEO_PATH"
-mv /home/hgrgustavo/Downloads/*.mov -t "$TRIAGE_VIDEO_PATH"
+#mv /home/hgrgustavo/Downloads/*.mov -t "$TRIAGE_VIDEO_PATH"
 
-generate_checksums()
-verify_checksums()
+generate_checksums
+verify_checksums
